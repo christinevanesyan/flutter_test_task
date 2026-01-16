c4b7db9bce55ab553d85892be80d79befa2e0faa
christine_vanesyan <christine@buycycle.com>
2026-01-16 11:37:02 +0400

    Init

---
+++ lib/app/flutter_test_task_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlutterTestTaskApp extends StatelessWidget {
+  const FlutterTestTaskApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test_task/app/flutter_test_task_app.dart';
+
+void main() {
+  runApp(const FlutterTestTaskApp());
+}
